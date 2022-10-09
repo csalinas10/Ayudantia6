f4f5fb6f4101761a652a74483335d88aa7a8df3b
csalinas10 <c.salinas10@ufromail.cl>
2022-10-09 18:41:08 -0300

    Final

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,3 +1,14 @@
-public class Launcher {
+import java.util.ArrayList;
 
+public class Launcher {
+    public static void main(String[] args) {
+        Registro registro = new Registro();
+        registro.agregarPersonas(45, "Pablo");
+        registro.agregarPersonas(65, "Lelo");
+        registro.agregarPersonas(50,"Rogelio");
+        registro.buscarPacienteNombre(registro.getPacientes());
+        registro.personasMayores(registro.getEdad());
+
+    }
 }
+
--- src/main/java/Registro.java
+++ src/main/java/Registro.java
@@ -1,3 +1,37 @@
-public class Registro {
+import java.util.ArrayList;
 
+public class Registro {
+    ArrayList<Persona> pacientes = new ArrayList<>();
+
+    public Persona agregarPersonas(int edad, String nombre) {
+        Persona personaNueva = new Persona(edad, nombre);
+        this.pacientes.add(personaNueva);
+        return personaNueva;
+    }
+    public void buscarPacienteNombre(ArrayList<Persona>pacientes){
+        String nombre = "Lelo";
+        for (Persona paciente : pacientes){
+            if (paciente.getNombre().equals(nombre)){
+                System.out.println("NOMBRE | EDAD");
+                System.out.println(paciente.toString());
+            }
+        }
+    }
+
+    public ArrayList<Persona> getPacientes() {
+        return pacientes;
+    }
+
+    public void personasMayores(ArrayList<Persona>pacientes) {
+        String nombre = "Rogelio";
+        int edad = 60;
+        for (Persona paciente : pacientes) {
+            if (pacientes > edad) {
+                System.out.println(paciente.toString());
+            }
+        }
+    }
+    public ArrayList<Persona> getEdad() {
+        return pacientes;
+    }
 }
